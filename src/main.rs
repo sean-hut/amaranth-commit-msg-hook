1e54b9748b5e0792dcb9b2b1594df7f9e5fc5667
Sean Hutchings <seanhut@yandex.com>
2020-10-22 20:22:11 -0400

    R add check for the command line argument
    Signed-off-by: Sean Hutchings <seanhut@yandex.com>

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,17 @@ use std::env::args;
 
 fn main() {
     let argument: Option<String> = args().nth(1);
+
+    check_argument(argument);
+}
+
+fn check_argument(argument: Option<String>) {
+    match argument {
+        Some(path_string) => read_commit_message(path_string),
+
+        None => {
+            eprintln!("Did not recieve an argument from git.");
+            exit(1);
+        }
+    }
 }
