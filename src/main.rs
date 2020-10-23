86e2440dec90faf318f032c19201f7b765dfc924
Sean Hutchings <seanhut@yandex.com>
2020-10-22 20:25:10 -0400

    R add check that the argument string can be parsed and read
    Signed-off-by: Sean Hutchings <seanhut@yandex.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use std::env::args;
+use std::fs::read_to_string;
+use std::path::Path;
+use std::process::exit;
+
+use amaranth_commit_msg_hook::check_commit_message;
 
 fn main() {
     let argument: Option<String> = args().nth(1);
@@ -12,6 +17,23 @@ fn check_argument(argument: Option<String>) {
 
         None => {
             eprintln!("Did not recieve an argument from git.");
+            exit(1);
+        }
+    }
+}
+
+fn read_commit_message(path_string: String) {
+    let path: &Path = Path::new(&path_string);
+    let commit_message_contents = read_to_string(path);
+
+    match commit_message_contents {
+        Ok(contents) => check_commit_message(contents),
+
+        Err(error) => {
+            eprintln!(
+                "Could not read file provided to git commit-msg hook: {}",
+                error
+            );
             exit(1);
         }
     }
