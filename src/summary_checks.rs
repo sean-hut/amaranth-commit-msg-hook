7c7bf971f136f1730fe2605903f82b7dc1a95b43
Sean Hutchings <seanhut@yandex.com>
2020-10-24 14:06:18 -0400

    R add function to get the first word of a commit's summary
    Signed-off-by: Sean Hutchings <seanhut@yandex.com>

--- src/summary_checks.rs
+++ src/summary_checks.rs
@@ -9,3 +9,13 @@ fn summary_line(content: &String) -> String {
         }
     }
 }
+
+fn first_summary_word(content: &String) -> String {
+    match summary_line(&content).split_ascii_whitespace().nth(1) {
+        Some(s) => s.to_string(),
+        None => {
+            eprintln!("ERROR: Commit message summary line needs to be more than just a category abbreviation.");
+            exit(2);
+        }
+    }
+}
