61b1f427ffe3cd3926c125740b616bf869cc9958
Sean Hutchings <seanhut@yandex.com>
2020-10-24 14:09:59 -0400

    AA add check that first word in summary in not capitalized
    Signed-off-by: Sean Hutchings <seanhut@yandex.com>

--- src/summary_checks.rs
+++ src/summary_checks.rs
@@ -19,3 +19,31 @@ fn first_summary_word(content: &String) -> String {
         }
     }
 }
+
+pub fn first_summary_word_not_lowercase(content: &String) -> Result<String, String> {
+    match first_summary_word(content).to_ascii_lowercase() == first_summary_word(content) {
+        true => Ok("First word in summary line is lowercase.".to_string()),
+        false => Err("First word in summary line is not lowercase.".to_string()),
+    }
+}
+
+#[cfg(test)]
+mod summary {
+
+    use super::{
+        first_summary_word_not_imperative_mood, first_summary_word_not_lowercase,
+        invalid_category_abbreviation, summary_ends_with_period, summary_over_50_characters,
+    };
+
+    use super::first_summary_word_not_lowercase;
+
+    #[test]
+    fn first_summary_word_not_lowercase_test() -> Result<(), &'static str> {
+        let not_lowercase_first_word: String = "D Add readme".to_string();
+
+        match first_summary_word_not_lowercase(&not_lowercase_first_word) {
+            Err(_) => Ok(()),
+            Ok(_) => Err("Did not error as expected."),
+        }
+    }
+}
